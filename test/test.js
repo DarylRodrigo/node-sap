53dfbbbf73ae7558b3ff0f31ab8761a00d23c408
Mic <mic.cassano@gmail.com>
2016-09-28 14:03:29 +0100

    Fix `Customers` test

--- test/test.js
+++ test/test.js
@@ -7,20 +7,21 @@ var should = require('chai').should();
 var sap = require('../index')(auth);
 
 describe('Access Token', function() {
-    this.timeout(30000);
+  this.timeout(30000);
 
-    it('Should create authentication token', function(done) {
-        setTimeout(function() {
-            should.exist(sap.access_token);
-            done();
-        }, 1000)
-    }); 
+  it('Should create authentication token', function(done) {
+    setTimeout(function() {
+      should.exist(sap.access_token);
+      done();
+    }, 1000);
+  });
 });
 
 describe('Customers', function() {
-    it('Should get all Customers', function(done) {
-        sap.getCustomers(function(err, res, body) {
-            expect(body.length).to.not.equal(0);
-        })
+  it('Should get all Customers', function(done) {
+    sap.getCustomers(function(err, res, body) {
+        expect(body.length).to.not.equal(0);
+        done();
     });
+  });
 });
